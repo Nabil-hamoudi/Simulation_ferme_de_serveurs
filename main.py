6c54558891d3a3c26fd5b96ed346b7cb7f4766b5
HAMOUDI nabil <nabil.hamoudi@ens.uvsq.fr>
2025-04-30 19:59:12 +0200

    Ajout code fonctionnel

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import heapq
 import numpy as np
 from groupe import Groupe
 from routeur import simul_fifo, duree_exp
-#from serveurs import liste_serveurs
+# from serveurs import liste_serveurs
 import serveur
 
 # Paramètres de simulation
 lambda_requete = 0.5  # Taux d'arrivée des requêtes
 ENSEMBLE_VALIDE_C = {1, 2, 3, 6}
-C = 2  # Nombre de groupes de serveurs (doit être dans ensemble_valide_C = {1, 2, 3, 6})
+# Nombre de groupes de serveurs (doit être dans ensemble_valide_C = {1, 2, 3, 6})
+C = 2
+
 
 def main():
     """Initialise un groupe de 12 serveurs et simule la file d'attente avec C donné"""
     if C not in ENSEMBLE_VALIDE_C:
         raise ValueError("C doit être un des éléments suivants: {1, 2, 3, 6}.")
-    
+
     # Initialisation des serveurs et des groupes
     liste_groupes = []
     for categorie in range(1, C + 1):
--- routeur.py
+++ routeur.py
@@ -1,8 +1,7 @@
 import heapq
-import random
 import numpy as np
 
-#from requetes import attribuer_type_requete
+# from requetes import attribuer_type_requete
 from requetes import Requete
 
 # capacité maximale
@@ -11,53 +10,67 @@ CAPACITE_MAX = 100
 PERTE_MAX = 0.05
 # durée maximale
 T_MAX = 100
+# nombre de serveurs
+NBR_SERVEURS = 12
+
 
 def duree_exp(lambda_requete):
     """ Génère une durée suivant la loi exponentielle en lambda """
     return np.random.exponential(1 / lambda_requete)
 
-def simul_fifo(lambda_requete, C, liste_groupes):
+
+def typeserveur(C):
+    """Genere une valeur correspondant au type de serveur"""
+    return np.random.randint(0, C)
+
+
+def simul_fifo(lambda_requete, C):
     """ Simule la file d'attente en fonction du taux d'arrivée lambda_requete """
+    # On initialise le nombre de serveur par type
+    nombre_serveurs_type = NBR_SERVEURS // C
+    # On initialise si serveur dispo ou non
+    serveurs = [0 for _ in range(C)]
+
     # Variables
     t = 0  # Temps actuel
     n = 0  # Nombre de clients à chaque instant
+
+    n_tot = 0  # nombre client total
+    n_drop = 0  # Nombre de clients drop
     echeancier = []  # Contient les événements à venir
-    
     # On initialise l'échéancier avec l'arrivée du premier client
-    heapq.heappush(echeancier, (0, Requete(C), "client"))
-    
+    heapq.heappush(echeancier, (0, typeserveur(C), "client"))
+    n += 1
+    n_tot += 1
     # Evolution du nombre de clients au cours du temps
     n_t = []
-    
+
     # Boucle principale de simulation
     while t < T_MAX:
         # On récupère les données à afficher
         n_t.append([t, n])
-        
+
         # On extrait l'événement le plus proche dans le temps
         if not echeancier:
             break
-        
         evt = heapq.heappop(echeancier)
         t = evt[0]
-        
         if evt[2] == "service":
             # Un client termine son service
             n -= 1
-            if n > 0:
-                heapq.heappush(echeancier, (t + duree_exp(1), None, "service"))
+            serveurs[evt[1]] -= 1
         elif evt[2] == "client":
-            # recherche du groupe correspondant à la requête, puis d'un serveur disponible dans ce groupe
-            for groupe in liste_groupes:
-                if groupe.categorie == evt[1].categorie:
-                    for serveur in groupe.serveurs:
-                        if serveur.est_occupe() == False:
-                            serveur.ajouter_requete(evt[1])
+            # regarde si plein
+            if serveurs[evt[1]] > nombre_serveurs_type:
+                n_drop += 1
+            else:
+                serveurs[evt[1]] += 1
+                heapq.heappush(
+                    echeancier, (t + duree_exp(1), evt[1], "service"))
             # Un nouveau client arrive
-            heapq.heappush(echeancier, (t + duree_exp(lambda_requete), Requete(C), "client"))
+            heapq.heappush(
+                echeancier, (t + duree_exp(lambda_requete), typeserveur(C), "client"))
             n += 1
-            if n == 1:
-                heapq.heappush(echeancier, (t + duree_exp(1), None, "service"))
-    
+
     # Retourne les statistiques mesurées
     return n_t
